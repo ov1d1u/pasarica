3f59c2f4a4280734f287f99d774b3605254a091a
Ovidiu D. Nitan <nitanovidiu@gmail.com>
2018-09-17 22:55:48 +0300

    Use SmoothADC to smooth value readings

--- Pasarica.ino
+++ Pasarica.ino
@@ -1,43 +1,63 @@
 #include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+#include <SmoothADC.h>
 
+SmoothADC adc;
 SoftwareSerial BTSerial(8, 7);  // RX, TX
-long previousMillis = 0;
-long interval = 5000;
+const size_t jsonBufferSize = JSON_OBJECT_SIZE(1);
+DynamicJsonDocument doc;
+
+unsigned long previousMillis = 0; 
+const long interval = 500;
+double lastTemp = 0.0;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Welcome! This is Pasarica v0.1a!");
+  Serial.println("Welcome! This is Pasarica v0.2a!");
   Serial.println("This device monitors the temperature of the hot water pipe");
   Serial.println("and sends the value to Home Assistant.");
   Serial.println("----------------------------------------------------------");
   Serial.println("Why \"Pasarica\"? Because \"O pasarica mi-a zis ca apa e gata de dus!\"");
   Serial.println("");
   BTSerial.begin(9600);
+  adc.init(1, TB_US, 500);
+  if (adc.isDisabled()) { 
+    adc.enable(); 
+  }
   Serial.println("Configuring Bluetooth LE module...");
-  BTSerial.write("AT+DEFAULT\r\n");
   BTSerial.write("AT+RESET\r\n");
   BTSerial.write("AT+NAME=Pasarica\r\n");
-  BTSerial.write("AT+ROLE1\r\n");
-  BTSerial.write("AT+TYPE\r\n");
   Serial.println("Done, starting the loop!");
 }
 
 void loop()
 {
+  adc.serviceADCPin();
+
   unsigned long currentMillis = millis();
-  if(currentMillis - previousMillis > interval) {
+  if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-    float temp = analogRead(1) * 0.48828125;
-    String jsonStr = String("{ \"temp\": ");
-    jsonStr += String(temp, 2);
-    jsonStr += String(" }\r\n");
-    char charBuffer[jsonStr.length() + 1];
-    jsonStr.toCharArray(charBuffer, jsonStr.length());
-    BTSerial.write(charBuffer);
-    //Serial.print(jsonStr);
+
+    double temp = round((adc.getADCVal() * 0.48828125)* 100) / 100.0;
+    if (fabs(lastTemp-temp) > 1.0) {
+      JsonObject root = doc.to<JsonObject>();
+      root["temp"] = temp;
+      serializeJson(doc, BTSerial);
+      lastTemp = temp;
+      //serializeJson(doc, Serial); Serial.println();
+    }
   }
-  if (BTSerial.available())
-    Serial.write(BTSerial.read());
-  if (Serial.available())
-    BTSerial.write(Serial.read());
+  
+  if (BTSerial.available()) {
+    String message = BTSerial.readStringUntil('\n');
+    if (message == "HELLO\r") {
+      // Client just connected, force resending of temperature
+      lastTemp = 0.0;
+    }
+  }
+
+//  if (BTSerial.available())
+//    Serial.write(BTSerial.read());
+//  if (Serial.available())
+//    BTSerial.write(Serial.read());
 }
